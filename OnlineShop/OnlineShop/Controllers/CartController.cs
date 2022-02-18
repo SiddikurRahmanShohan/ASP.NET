3c65eb8c1fef511ff10626d8acb182d28749fe59
SiddikurRahmanShohan <siddikurrahman.shohan@gmail.com>
2022-02-18 13:00:18 +0600

    Modified Online Shop

--- OnlineShop/OnlineShop/Controllers/CartController.cs
+++ OnlineShop/OnlineShop/Controllers/CartController.cs
@@ -13,7 +13,8 @@ namespace OnlineShop.Controllers
         //
         // GET: /Cart/
         EShopEntities db = new EShopEntities();
-        static Cart crt;
+         static Cart crt;
+         static int crtId;
         [HttpGet]
         public ActionResult Index()
         {
@@ -34,9 +35,10 @@ namespace OnlineShop.Controllers
                     crtItems.ItemPrice = ci.ItemPrice;
                     db.Cart_Itemes.Add(crtItems);
                     db.SaveChanges();
+                    UpdateQuantity(ci.Pid, ci.Qty);
                 }
                 Order o = new Order();
-                o.Cid = (from c in db.Carts where (c.Uid == 2 && c.Id == crtItems.Cid) select c.Id).FirstOrDefault();
+                o.Cid = crtId;
                 o.Uid = 2;
                 o.CreationDate = System.DateTime.Now;
                 o.Status = "Processing";
@@ -45,7 +47,6 @@ namespace OnlineShop.Controllers
                 o.TotalPrice = (from c in db.Cart_Itemes where (c.Cid == o.Cid) select (c.ItemPrice)).Sum();
                 db.Orders.Add(o);
                 db.SaveChanges();
-                //var cartTot = (from ct in db.Carts where ct.Oid == o.Id select ct);
 
                 Session["cart"] = null;
                 return RedirectToAction("Index", "Product");
@@ -59,35 +60,43 @@ namespace OnlineShop.Controllers
         {
             var product = (from p in db.Products where p.Id == id select p).FirstOrDefault();
             string cartJson;
-            if (Session["cart"] == null)
+            if (CheckQty(id))
             {
-                crt = new Cart();
-                crt.Date = System.DateTime.Now;
-                crt.Uid = 2;
-                db.Carts.Add(crt);
-                db.SaveChanges();
-                List<Cart_Itemes> cartItems = new List<Cart_Itemes>();
-
-                cartItems.Add(new Cart_Itemes { Cid = (from c in db.Carts where (c.Uid == 2 && c.Id == crt.Id) select c.Id).FirstOrDefault(), Pid = id, Qty = 1, ItemPrice = product.SellPrice });
-                cartJson = new JavaScriptSerializer().Serialize(cartItems);
-                Session["cart"] = cartJson;
-            }
-            else
-            {
-                var cartDes = new JavaScriptSerializer().Deserialize<List<Cart_Itemes>>(Session["cart"].ToString());
-                int index = isExist(id);
-                if (index != -1)
+                if (Session["cart"] == null)
                 {
-                    cartDes[index].Qty++;
-                    cartDes[index].ItemPrice = (cartDes[index].Qty * product.SellPrice);
+                    crt = new Cart();
+                    crt.Date = System.DateTime.Now;
+                    crt.Uid = 2;
+                    db.Carts.Add(crt);
+                    db.SaveChanges();
+                    crtId = (from c in db.Carts where c.Uid == 2 orderby c.Id descending select c.Id).Take(1).SingleOrDefault();
+                    List<Cart_Itemes> cartItems = new List<Cart_Itemes>();
+
+                    cartItems.Add(new Cart_Itemes { Cid = crtId, Pid = id, Qty = 1, ItemPrice = product.SellPrice });
+                    cartJson = new JavaScriptSerializer().Serialize(cartItems);
+                    Session["cart"] = cartJson;
                 }
                 else
                 {
-                    cartDes.Add(new Cart_Itemes { Cid = (from c in db.Carts where (c.Uid == 2 && c.Id == crt.Id) select c.Id).FirstOrDefault(), Pid = id, Qty = 1, ItemPrice = product.SellPrice });
+                    var cartDes = new JavaScriptSerializer().Deserialize<List<Cart_Itemes>>(Session["cart"].ToString());
+                    int index = isExist(id);
+                    if (index != -1)
+                    {
+                        cartDes[index].Qty++;
+                        cartDes[index].ItemPrice = (cartDes[index].Qty * product.SellPrice);
+                    }
+                    else
+                    {
+                        cartDes.Add(new Cart_Itemes { Cid = crtId, Pid = id, Qty = 1, ItemPrice = product.SellPrice });
+                    }
+                    cartJson = new JavaScriptSerializer().Serialize(cartDes);
+                    Session["cart"] = cartJson;
                 }
-                cartJson = new JavaScriptSerializer().Serialize(cartDes);
-                Session["cart"] = cartJson;
             }
+            else {
+                return RedirectToAction("Index","Product");
+            }
+            
             return RedirectToAction("Index");
         }
         public ActionResult Remove(int id)
@@ -107,6 +116,32 @@ namespace OnlineShop.Controllers
                 if (cartDes[i].Pid.Equals(id))
                     return i;
             return -1;
+        }
+        private void UpdateQuantity(int id, int nqty)
+        {
+            var product = (from p in db.Products where p.Id == id select p).FirstOrDefault();
+            Product newProduct = new Product();
+            newProduct.Id = product.Id;
+            newProduct.Name = product.Name;
+            newProduct.Category = product.Category;
+            newProduct.SellPrice = product.SellPrice;
+            newProduct.PurchasePrice = product.PurchasePrice;
+            newProduct.Quantity = (product.Quantity - nqty);
+            db.Entry(product).CurrentValues.SetValues(newProduct);
+            db.SaveChanges();
+        }
+
+        private bool CheckQty(int id)
+        {
+            var product = (from p in db.Products where p.Id == id select p).FirstOrDefault();
+            if (product.Quantity > 0)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
         }
 	}
 }
