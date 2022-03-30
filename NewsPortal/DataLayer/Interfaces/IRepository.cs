b5746ec1e4e86f772dd3a790f635142b18993b2d
SiddikurRahmanShohan <siddikurrahman.shohan@gmail.com>
2022-03-30 22:41:58 +0600

    NewsPortal Assignment initialized

---
+++ NewsPortal/DataLayer/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Interfaces
+{
+    public interface IRepository<T, ID>
+    {
+        T Add(T obj);
+        T Get(ID id);
+        List<T> Get();
+        bool Edit(T obj);
+        bool Delete(ID id);
+    }
+}
